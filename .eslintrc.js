e209350dcf8312b59c3545b17c739bf0608a0890
bhavyasf <37434313+bhavyasf@users.noreply.github.com>
2022-09-27 17:10:18 +0530

    work in progress: code refactor

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': 'google',
+  'overrides': [
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  'rules': {
+    'require-jsdoc': 0,
+    'valid-jsdoc': 0,
+  },
+};
